84f107da548b697814aa4cc36b174fedced1b5ba
aguspiza <aguspiza@yahoo.es>
2019-11-29 20:55:55 +0100

    make code sorter

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ enum GameResult {
 
 #[derive(PartialEq)]
 #[derive(Debug)]
-enum GameScore {
+enum PlayerPoint {
     P1, P2
 }
 
@@ -27,18 +27,18 @@ struct Game {
 }
 
 impl Game {
-    fn score(&mut self, update: &GameScore) {
+    fn score(&mut self, update: &PlayerPoint) {
         if self.result != GameResult::OnGoing {
             return;
         }
 
         match update {
-            GameScore::P1 => {
+            PlayerPoint::P1 => {
                 let (p1, p2) = Game::adv_score(self.player_1, self.player_2);
                 self.player_1 = p1;
                 self.player_2 = p2;
             },
-            GameScore::P2 => {
+            PlayerPoint::P2 => {
                 let (p2, p1) = Game::adv_score(self.player_2, self.player_1);
                 self.player_1 = p1;
                 self.player_2 = p2;
@@ -58,30 +58,14 @@ impl Game {
 
     fn adv_score(player: Point, opponent: Point) -> (Point, Point) {
         match (&player, &opponent) {
-            (Point::PWin, _) => {
-                (Point::PWin, Point::P0)
-            },
-            (Point::PAdv, _) => {
-                (Point::PWin, Point::P0)
-            },
-            (Point::P40, Point::PAdv) => {
-                (Point::P40, Point::P40)
-            },
-            (Point::P40, Point::P40) => {
-                (Point::PAdv, opponent)
-            },
-            (Point::P40, _) => {
-                (Point::PWin, Point::P0)
-            },
-            (Point::P30, _) => {
-                (Point::P40, opponent)
-            },
-            (Point::P15, _) => {
-                (Point::P30, opponent)
-            },
-            (Point::P0, _) => {
-                (Point::P15, opponent)
-            }
+            (Point::PWin, _) => (Point::PWin, Point::P0),
+            (Point::PAdv, _) => (Point::PWin, Point::P0),
+            (Point::P40, Point::PAdv) => (Point::P40, Point::P40),
+            (Point::P40, Point::P40) => (Point::PAdv, opponent),
+            (Point::P40, _) => (Point::PWin, Point::P0),
+            (Point::P30, _) => (Point::P40, opponent),
+            (Point::P15, _) => (Point::P30, opponent),
+            (Point::P0, _) => (Point::P15, opponent)
         }
     }
 
@@ -128,14 +112,14 @@ mod tests {
     }
 
     fn generic_tennis_test_p1(p1: Point, p2: Point, p1_result: Point, p2_result: Point, is_win: bool) {
-        generic_tennis_test(p1, p2, GameScore::P1, p1_result, p2_result, if is_win { GameResult::P1Win } else { GameResult::OnGoing})
+        generic_tennis_test(p1, p2, PlayerPoint::P1, p1_result, p2_result, if is_win { GameResult::P1Win } else { GameResult::OnGoing})
     }
 
     fn generic_tennis_test_p2(p1: Point, p2: Point, p1_result: Point, p2_result: Point, is_win: bool) {
-        generic_tennis_test(p2, p1, GameScore::P2, p2_result, p1_result, if is_win { GameResult::P2Win } else { GameResult::OnGoing})
+        generic_tennis_test(p2, p1, PlayerPoint::P2, p2_result, p1_result, if is_win { GameResult::P2Win } else { GameResult::OnGoing})
     }
 
-    fn generic_tennis_test(p1: Point, p2: Point, update: GameScore, p1_result: Point, p2_result: Point, game_result: GameResult) {
+    fn generic_tennis_test(p1: Point, p2: Point, update: PlayerPoint, p1_result: Point, p2_result: Point, game_result: GameResult) {
         let mut actual_game = Game { player_1: p1, player_2: p2, result: GameResult::OnGoing };
         actual_game.score(&update);
         let expected_game = Game { player_1: p1_result, player_2: p2_result, result: game_result };
