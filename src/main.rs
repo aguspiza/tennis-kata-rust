cfc2cc40fd57bb66ebd704d8e008e30a49c88ff8
aguspiza <aguspiza@yahoo.es>
2019-11-29 19:56:09 +0100

    easier to reason version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #[derive(Copy)]
 #[derive(Debug)]
 enum Point {
-    P0, P15, P30, P40, PAdv
+    P0, P15, P30, P40, PAdv, PWin
 }
 
 #[derive(PartialEq)]
@@ -14,8 +14,8 @@ enum GameResult {
 
 #[derive(PartialEq)]
 #[derive(Debug)]
-enum GameUpdate {
-    P1Score, P2Score
+enum GameScore {
+    P1, P2
 }
 
 #[derive(PartialEq)]
@@ -26,58 +26,65 @@ struct Game {
     result: GameResult
 }
 
-fn update_game(game: &mut Game, update: &GameUpdate) {
-    if game.result != GameResult::OnGoing {
-        return;
+impl Game {
+    fn score(&mut self, update: &GameScore) {
+        if self.result != GameResult::OnGoing {
+            return;
+        }
+
+        match update {
+            GameScore::P1 => {
+                let (p1, p2) = Game::adv_score(self.player_1, self.player_2);
+                self.player_1 = p1;
+                self.player_2 = p2;
+            },
+            GameScore::P2 => {
+                let (p2, p1) = Game::adv_score(self.player_2, self.player_1);
+                self.player_1 = p1;
+                self.player_2 = p2;
+            }
+        }
+
+        self.check_winner();
     }
 
-    match update {
-        GameUpdate::P1Score => {
-            adv_p1_score(game);
-        },
-        GameUpdate::P2Score => {
-            swap_score(game);
-            adv_p1_score(game);
-            swap_score(game);
+    fn check_winner(&mut self) {
+        self.result = match (&self.player_1, &self.player_2) {
+            (Point::PWin, _) => GameResult::P1Win,
+            (_, Point::PWin) => GameResult::P2Win,
+            _ => GameResult::OnGoing
         }
     }
-}
 
-fn swap_score(game: &mut Game) {
-    let temp = game.player_1;
-    game.player_1 = game.player_2;
-    game.player_2 = temp;
-    game.result = match game.result {
-        GameResult::P1Win => GameResult::P2Win,
-        GameResult::P2Win => GameResult::P1Win,
-        GameResult::OnGoing => GameResult::OnGoing
-    }
-}
-
-fn adv_p1_score(game: &mut Game) {
-    match (&game.player_1, &game.player_2) {
-        (Point::PAdv, _) => {
-            game.result = GameResult::P1Win;
-        },
-        (Point::P40, Point::PAdv) => {
-            game.player_2 = Point::P40;
-        },
-        (Point::P40, Point::P40) => {
-            game.player_1 = Point::PAdv;
-        },
-        (Point::P40, _) => {
-            game.result = GameResult::P1Win;
-        },
-        (Point::P30, _) => {
-            game.player_1 = Point::P40;
-        },
-        (Point::P15, _) => {
-            game.player_1 = Point::P30;
-        },
-        (Point::P0, _) => {
-            game.player_1 = Point::P15;
+    fn adv_score(player: Point, opponent: Point) -> (Point, Point) {
+        match (&player, &opponent) {
+            (Point::PWin, _) => {
+                (Point::PWin, Point::P0)
+            },
+            (Point::PAdv, _) => {
+                (Point::PWin, Point::P0)
+            },
+            (Point::P40, Point::PAdv) => {
+                (Point::P40, Point::P40)
+            },
+            (Point::P40, Point::P40) => {
+                (Point::PAdv, opponent)
+            },
+            (Point::P40, _) => {
+                (Point::PWin, Point::P0)
+            },
+            (Point::P30, _) => {
+                (Point::P40, opponent)
+            },
+            (Point::P15, _) => {
+                (Point::P30, opponent)
+            },
+            (Point::P0, _) => {
+                (Point::P15, opponent)
+            }
         }
     }
+
 }
 
 #[cfg(test)]
@@ -89,16 +96,15 @@ mod tests {
         generic_tennis_test_p1(Point::P0, Point::P0, Point::P15, Point::P0, false);
         generic_tennis_test_p1(Point::P15, Point::P0, Point::P30, Point::P0, false);
         generic_tennis_test_p1(Point::P30, Point::P0, Point::P40, Point::P0, false);
-        generic_tennis_test_p1(Point::P40, Point::P0, Point::P40, Point::P0, true);
+        generic_tennis_test_p1(Point::P40, Point::P0, Point::PWin, Point::P0, true);
         generic_tennis_test_p1(Point::P15, Point::P15, Point::P30, Point::P15, false);
         generic_tennis_test_p1(Point::P30, Point::P15, Point::P40, Point::P15, false);
-        generic_tennis_test_p1(Point::P40, Point::P15, Point::P40, Point::P15, true);
+        generic_tennis_test_p1(Point::P40, Point::P15, Point::PWin, Point::P0, true);
         generic_tennis_test_p1(Point::P15, Point::P30, Point::P30, Point::P30, false);
         generic_tennis_test_p1(Point::P30, Point::P30, Point::P40, Point::P30, false);
-        generic_tennis_test_p1(Point::P40, Point::P30, Point::P40, Point::P30, true);
+        generic_tennis_test_p1(Point::P40, Point::P30, Point::PWin, Point::P0, true);
         generic_tennis_test_p1(Point::P15, Point::P40, Point::P30, Point::P40, false);
         generic_tennis_test_p1(Point::P30, Point::P40, Point::P40, Point::P40, false);
-        generic_tennis_test_p1(Point::P40, Point::P40, Point::PAdv, Point::P40, false);
         generic_tennis_test_p1(Point::P40, Point::P40, Point::PAdv, Point::P40, false);
         generic_tennis_test_p1(Point::P40, Point::PAdv, Point::P40, Point::P40, false);
     }
@@ -108,31 +114,30 @@ mod tests {
         generic_tennis_test_p2(Point::P0, Point::P0, Point::P15, Point::P0, false);
         generic_tennis_test_p2(Point::P15, Point::P0, Point::P30, Point::P0, false);
         generic_tennis_test_p2(Point::P30, Point::P0, Point::P40, Point::P0, false);
-        generic_tennis_test_p2(Point::P40, Point::P0, Point::P40, Point::P0, true);
+        generic_tennis_test_p2(Point::P40, Point::P0, Point::PWin, Point::P0, true);
         generic_tennis_test_p2(Point::P15, Point::P15, Point::P30, Point::P15, false);
         generic_tennis_test_p2(Point::P30, Point::P15, Point::P40, Point::P15, false);
-        generic_tennis_test_p2(Point::P40, Point::P15, Point::P40, Point::P15, true);
+        generic_tennis_test_p2(Point::P40, Point::P15, Point::PWin, Point::P0, true);
         generic_tennis_test_p2(Point::P15, Point::P30, Point::P30, Point::P30, false);
         generic_tennis_test_p2(Point::P30, Point::P30, Point::P40, Point::P30, false);
-        generic_tennis_test_p2(Point::P40, Point::P30, Point::P40, Point::P30, true);
+        generic_tennis_test_p2(Point::P40, Point::P30, Point::PWin, Point::P0, true);
         generic_tennis_test_p2(Point::P15, Point::P40, Point::P30, Point::P40, false);
         generic_tennis_test_p2(Point::P30, Point::P40, Point::P40, Point::P40, false);
-        generic_tennis_test_p2(Point::P40, Point::P40, Point::PAdv, Point::P40, false);
         generic_tennis_test_p2(Point::P40, Point::P40, Point::PAdv, Point::P40, false);
         generic_tennis_test_p2(Point::P40, Point::PAdv, Point::P40, Point::P40, false);
     }
 
     fn generic_tennis_test_p1(p1: Point, p2: Point, p1_result: Point, p2_result: Point, is_win: bool) {
-        generic_tennis_test(p1, p2, GameUpdate::P1Score, p1_result, p2_result, if is_win { GameResult::P1Win } else { GameResult::OnGoing})
+        generic_tennis_test(p1, p2, GameScore::P1, p1_result, p2_result, if is_win { GameResult::P1Win } else { GameResult::OnGoing})
     }
 
     fn generic_tennis_test_p2(p1: Point, p2: Point, p1_result: Point, p2_result: Point, is_win: bool) {
-        generic_tennis_test(p2, p1, GameUpdate::P2Score, p2_result, p1_result, if is_win { GameResult::P2Win } else { GameResult::OnGoing})
+        generic_tennis_test(p2, p1, GameScore::P2, p2_result, p1_result, if is_win { GameResult::P2Win } else { GameResult::OnGoing})
     }
 
-    fn generic_tennis_test(p1: Point, p2: Point, update: GameUpdate, p1_result: Point, p2_result: Point, game_result: GameResult) {
+    fn generic_tennis_test(p1: Point, p2: Point, update: GameScore, p1_result: Point, p2_result: Point, game_result: GameResult) {
         let mut actual_game = Game { player_1: p1, player_2: p2, result: GameResult::OnGoing };
-        update_game(&mut actual_game, &update);
+        actual_game.score(&update);
         let expected_game = Game { player_1: p1_result, player_2: p2_result, result: game_result };
         assert_eq!(&actual_game, &expected_game);
     }
